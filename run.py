60a4e2fe6ad7fe43bd4b3e86e3ef5c5cdc596de4
Richard Blaauw <r.t.blaauw@live.com>
2021-09-04 17:14:08 +0000

    Update run.py

--- run.py
+++ run.py
@@ -48,8 +48,8 @@ def careers():
 
 if __name__ == "__main__":
     app.run(
-     host=os.environ.get("IP", "0.0.0.0"),
-     port=int(os.environ.get("Port", 5000)),
+     host=os.environ.get("IP"),
+     port=int(os.environ.get("PORT")),
      debug=True
     )
 
